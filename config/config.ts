60ee360fe4019f05dd1803865ab43c46a8351dd2
roman <sk.trac@yandex.ru>
2023-02-08 18:17:31 +0300

    deleted config

--- config/config.ts
+++
@@ -1,7 +0,0 @@
-import { exampleImportsTransform, exampleTransform } from "../transform/example";
-import { Config } from '../types/types';
-
-export const config: Config = {
-  transformFunction: [exampleImportsTransform, exampleTransform],
-  filesRegex: /\.ts$/,
-};
