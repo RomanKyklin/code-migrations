e733daffa8002a954eecf1f6484226583b56aa3b
roman <sk.trac@yandex.ru>
2023-02-15 17:08:50 +0300

    example.ts updated

--- transform/example.ts
+++ transform/example.ts
@@ -1,52 +1,77 @@
 import { Visitor } from "@babel/core";
 import { Types, TransformFunction } from "../types/types";
-import { Identifier } from "@babel/types";
+import {
+  Identifier,
+  identifier,
+  importDeclaration,
+  importSpecifier,
+  stringLiteral,
+} from "@babel/types";
 
-const transform: TransformFunction = (t: Types): Visitor => ({
-  // transform type UUID to type NullUuid
-  TSTypeReference(path) {
-    const typeName = path.node.typeName as Identifier;
+const transform: TransformFunction = (t: Types): Visitor => {
+  const newImportDeclaration = importDeclaration(
+    [
+      importSpecifier(
+        identifier("SOME_CONSTANT_1"),
+        identifier("SOME_CONSTANT_1")
+      ),
+      importSpecifier(
+        identifier("SOME_CONSTANT_2"),
+        identifier("SOME_CONSTANT_2")
+      ),
+      importSpecifier(
+        identifier("SOME_CONSTANT_3"),
+        identifier("SOME_CONSTANT_3")
+      ),
+    ],
+    stringLiteral("@bimeister/frontend.constants")
+  );
 
-    if (typeName.name === "Uuid") {
-      typeName.name = "NullUuid";
-    }
-  },
-  ImportDeclaration(path) {
-    // replace imports
-    const source = path.node.source.value;
-    if (
-      source === "@app/constants/some-constant-1.const" ||
-      source === "@app/constants/some-constant-2.const" ||
-      source === "@app/constants/some-constant-3.const"
-    ) {
-      path.replaceWith(
-        t.importDeclaration(
-          path.node.specifiers,
-          t.stringLiteral("@bimeister/frontend.constants")
-        )
-      );
-    }
+  return {
+    Program(path) {
+      path.unshiftContainer("body", newImportDeclaration);
+    },
+    // transform type UUID to type NullUuid
+    TSTypeReference(path) {
+      const typeName = path.node.typeName as Identifier;
 
-    if (path.node.source.value === "@app/declarations/types/uuid.type") {
-      path.replaceWith(
-        t.importDeclaration(
-          path.node.specifiers,
-          t.stringLiteral("@bimeister/types/uuid.type")
-        )
-      );
+      if (typeName.name === "Uuid") {
+        typeName.name = "NullUuid";
+      }
+    },
+    ImportDeclaration(path) {
+      // remove imports
+      const source = path.node.source.value;
+      if (
+        source === "@app/constants/some-constant-1.const" ||
+        source === "@app/constants/some-constant-2.const" ||
+        source === "@app/constants/some-constant-3.const"
+      ) {
+        path.remove();
+        return
+      }
 
-      path.node.specifiers.map((specifier) => {
-        if (
-          t.isImportSpecifier(specifier) &&
-          t.isIdentifier(specifier.imported) &&
-          specifier.imported.name === "Uuid"
-        ) {
-          specifier.imported.name = "NullUuid";
-          specifier.local.name = "NullUuid";
-        }
-      });
-    }
-  },
-});
+      if (path.node.source.value === "@app/declarations/types/uuid.type") {
+        path.replaceWith(
+          t.importDeclaration(
+            path.node.specifiers,
+            t.stringLiteral("@bimeister/types/uuid.type")
+          )
+        );
+
+        path.node.specifiers.map((specifier) => {
+          if (
+            t.isImportSpecifier(specifier) &&
+            t.isIdentifier(specifier.imported) &&
+            specifier.imported.name === "Uuid"
+          ) {
+            specifier.imported.name = "NullUuid";
+            specifier.local.name = "NullUuid";
+          }
+        });
+      }
+    },
+  };
+};
 
 export default transform;
